3732b9dd22113d8cc0c4dc0db8c9cc4ff2db3b7b
Andrew Ross <andrew.ross@jembi.org>
2017-10-04 09:29:14 +0200

    Changed a line

--- react-router.d.ts
+++ react-router.d.ts
@@ -3,6 +3,7 @@
 import * as React from "react";
 import {LocationDescriptor, Query, Location, History, HistoryOptions, CreateHistory, BasenameOptions, QueryOptions} from "history";
 
+
 declare module ReactRouter {
 
 	type RouteConfig = Route | PlainRoute;
